06159260650150a00de73daf641d890bc6d90654
Steven King <email": null>
2016-02-23 16:32:28 -0500

    user auth is working

--- config/environment.rb
+++ config/environment.rb
@@ -17,8 +17,10 @@ require 'logger'
 
 require 'sinatra'
 require "sinatra/reloader" if development?
-require 'bcrypt'
+
 require 'erb'
+require 'bcrypt'
+
 
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
